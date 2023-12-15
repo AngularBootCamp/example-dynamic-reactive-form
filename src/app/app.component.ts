f1a7b851619fbf10c0dba3d7cd86754444bff4a8
Lance Finney <lance.finney@oasisdigital.com>
2023-12-15 16:41:05 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { SchemaFormUtils } from './schema-form-utils';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrl: './app.component.scss',
   standalone: true,
   imports: [ReactiveFormsModule, NgFor, NgIf, JsonPipe]
 })
